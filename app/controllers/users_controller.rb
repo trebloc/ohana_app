0ecdcb69b1688f63ce22675aef8b238bcf248c07
trebloc <ctse77@alum.berkeley.edu>
2016-02-18 15:58:55 -0800

    setting up signup for users in controller and signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,12 @@ class UsersController < ApplicationController
   def new
     @user = User.new
     render :new
-  end    
+  end
+
+  def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
+    @user = User.create(user_params)
+
+    redirect_to root_path
+  end      
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "welcome#index"  
 
   get "/users/new", to: "users#new", as: "new_user"
+  post "/users", to: "users#create"
 end
