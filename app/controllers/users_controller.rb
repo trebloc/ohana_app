16c1b639041f411a23bd522f9de4d74654d564d5
trebloc <ctse77@alum.berkeley.edu>
2016-02-18 16:16:03 -0800

    finialized login for show, sessions, and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,8 @@ class UsersController < ApplicationController
   def create
     user_params = params.require(:user).permit(:first_name, :last_name, :email, :password)
     @user = User.create(user_params)
-
-    redirect_to root_path
+    login(@user) # <-- login the user
+    redirect_to @user # <-- go to show
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   get "/users/new", to: "users#new", as: "new_user"
   post "/users", to: "users#create"
   get "/users/:id", to: "users#show", as: "user"
-  
-  get "/login", to: "sessions#new"    
+
+  get "/login", to: "sessions#new"
+  get "/logout", to: "sessions#destroy" # <-- strictly speaking this isn't RESTful (it should be a DELETE not GET), but it's super conveient to do it this way
+  post "/sessions", to: "sessions#create"   
 end
